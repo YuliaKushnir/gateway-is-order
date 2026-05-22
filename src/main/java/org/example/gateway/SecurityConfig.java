c70cbb1dd538dbfbb93476db9b8a8b128a4a26f8
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-22 10:26:09 +0300

    temporary

--- src/main/java/org/example/gateway/SecurityConfig.java
+++ src/main/java/org/example/gateway/SecurityConfig.java
@@ -49,7 +49,8 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.POST, "/api/stocks/**").hasAnyAuthority("ROLE_MANAGER", "ROLE_ADMIN")
 
                         .pathMatchers("/actuator/**").permitAll()
-                        .anyExchange().authenticated()
+//                        .anyExchange().authenticated()
+                                .anyExchange().permitAll()
                 )
 //                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .oauth2ResourceServer(oauth2 ->
